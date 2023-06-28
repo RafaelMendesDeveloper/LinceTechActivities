ff7602e0757bc07198d5aa25f0ed3d04fafbdff6
RafaelMendesDeveloper <rafael.mendes.negocios@gmail.com>
2023-06-28 15:42:42 -0300

    primeiro exercicio tarefa 6 realizados

---
+++ ed6/ed6_ap1.dart
@@ -0,0 +1,18 @@
+// Crie uma funcao que receba uma string e converta essas string para inteiro.
+// Utilize uma estrutura try-catch para tratar possiveis erros ao converter a entrada do usuario para um numero inteiro.
+// Se ocorrer um erro, exiba a mensagem "Entrada invalida. Insira apenas numeros inteiros." Caso contrario, exiba o numero digitado.
+
+// Exemplo de saida com erro:
+// Entrada invalida. Digite apenas numeros inteiros.
+// Exemplo de saida bem sucedida:
+// Numero digitado: 9999
+
+void main() {
+  try {
+    final texto = '32';
+    final StringConvertida = int.parse(texto);
+    print('Numero digitado: $StringConvertida');
+  } catch (e) {
+    print('Entrada invalida. Insira apenas numeros inteiros.');
+  }
+}
