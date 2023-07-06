d10f5f4d9d55547a8d848298136b22b26f3cb2fe
RafaelMendesDeveloper <rafael.mendes.negocios@gmail.com>
2023-07-06 15:38:37 -0300

    exercicio realizado

---
+++ ed9/ed9_ap1.dart
@@ -0,0 +1,36 @@
+// AP1 - Modifique o programa base utilizando padroes da programacao assincrona para poder retornar o resultado
+// Inicio
+// Status de entrega do pacote ...
+// Status de entrega do pacote ...
+// Status de entrega do pacote ...
+// Fim
+
+import 'dart:async';
+
+Future<String> consultarAPI(String codigoRastreamento) async {
+  await Future.delayed(Duration(seconds: 2));
+
+  // Simulacao de consulta a API com codigo de rastreamento
+  if (codigoRastreamento == 'ABC123') {
+    return 'Em transito';
+  } else if (codigoRastreamento == 'XYZ789') {
+    return 'Entregue';
+  } else if (codigoRastreamento == 'DEF456') {
+    return 'Atrasado';
+  } else {
+    return 'Codigo de rastreamento invalido';
+  }
+}
+
+Future<void> verificarStatusEntrega(String codigoRastreamento) async {
+  String status = await consultarAPI(codigoRastreamento);
+  print('Status de entrega do pacote $codigoRastreamento: $status');
+}
+
+void main ()async {
+  print('Inicio');
+  await verificarStatusEntrega('ABC123');
+  await verificarStatusEntrega('XYZ789');
+  await verificarStatusEntrega('DEF456');
+  print('Fim');
+}
