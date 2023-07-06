9660325c208297a73eb7000c8c87735501b46f99
RafaelMendesDeveloper <rafael.mendes.negocios@gmail.com>
2023-07-06 18:17:04 -0300

    nomes e sobrenomes aleatórios

---
+++ DesafiosDecolaDart1/d1_de5.dart
@@ -0,0 +1,79 @@
+// Escreva um programa que gere uma sugestao de nome completo aleatorio, com base em listas pre-definidas de nomes e sobrenomes.
+
+// Lista de nomes:
+// Ana, Maria, Francisco, Joao, Pedro, Gabriel, Rafaela, Marcio, Jose, Carlos, Patricia, Helena, Camila,
+// Mateus, Gabriel, Samuel, Karina, Antonio, Daniel, Joel, Cristiana, Sebastiao, Paula
+
+// Lista de sobrenomes: Silva, Souza, Almeida, Azevedo, Braga, Barros, Campos, Cardoso, Costa, Teixeira, Santos,
+// Rodrigues, Ferreira, Alves, Pereira, Lima, Gomes, Ribeiro, Carvalho, Lopes, Barbosa
+
+// Tempo do desafio: 10 minutos
+
+// Inicio 18:07
+
+import 'dart:math';
+
+void main() {
+  final List<String> nomes = [
+    'Ana',
+    'Maria',
+    'Francisco',
+    'Joao',
+    'Pedro',
+    'Gabriel',
+    'Rafaela',
+    'Marcio',
+    'Jose',
+    ' Carlos',
+    'Patricia',
+    'Helena',
+    'Camila',
+    'Mateus',
+    'Gabriel',
+    'Samuel',
+    'Karina',
+    'Antonio',
+    'Daniel',
+    'Joel',
+    'Cristiana',
+    'Sebastiao',
+    'Paula'
+  ];
+
+  final List<String> sobrenomes = [
+    'Silva',
+    'Souza',
+    'Almeida',
+    'Azevedo',
+    'Braga',
+    'Barros',
+    'Campos',
+    'Cardoso',
+    'Costa',
+    'Teixeira',
+    'Santos',
+    'Rodrigues',
+    'Ferreira',
+    'Alves',
+    'Pereira',
+    'Lima',
+    'Gomes',
+    'Ribeiro',
+    'Carvalho',
+    'Lopes',
+    'Barbosa'
+  ];
+
+  int numNome = gerarNumeroAleatorio(nomes);
+  int numSobrenome = gerarNumeroAleatorio(sobrenomes);
+
+  print('Nome sorteado = ${nomes[numNome]} ${sobrenomes[numSobrenome]}');
+}
+
+gerarNumeroAleatorio(List<String> lista) {
+  final random = Random();
+  final num1 = random.nextInt(lista.length);
+  return num1;
+}
+
+//Fim 18:14
