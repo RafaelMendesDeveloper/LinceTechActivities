bf4b973a76f878e2ef992ce6e11785b83429c2ad
RafaelMendesDeveloper <rafael.mendes.negocios@gmail.com>
2023-07-12 16:01:07 -0300

    todos os exercícios realizados

--- DesafiosDecolaDart2/d2_de3.dart
+++ DesafiosDecolaDart2/d2_de3.dart
@@ -8,44 +8,81 @@
 // Nome do album
 // Duracao em segundos
 // A biblioteca deve permitir buscar musica pelo titulo, nome dor artista e nome do album.
-// Deve ser incluido no programa a execucao de todas essas buscas de utilizando esses parametros apos imprimir a biblioteca.
-
+// Deve ser incluido no programa a execucao de todas essas buscas de utilizando esses parametros apos imprimir a biblioteca
 // Tempo do desafio: 60 minutos
-
 //Inicio 11:30
 void main() {
-  final musica1 =
-      musicas('Estilo Cachorro', 'ManoBrown', 'Sobrevivendo no Inferno', 920); 
-  List ListMusica1 = musica1.adicionarLista();
-  Biblioteca().adicionarMusicas(musica1.titulo, ListMusica1);
-  final musica2 =
-      musicas('Flamengo', 'Jorge Jesus', 'Coringas do Mengão', 157);
-  List ListMusica2 = musica2.adicionarLista();
-  Biblioteca().adicionarMusicas(musica2.titulo, ListMusica2);
-  print('Musicas Cadastradas: ${Biblioteca().listaDeMusicas.keys}');
+  final biblioteca = Biblioteca();
+
+  // Cadastro das músicas
+  biblioteca.cadastrarMusica(Musica('Estilo Cachorro', 'ManoBrown', 'Sobrevivendo no Inferno', 920));
+  biblioteca.cadastrarMusica(Musica('Flamengo', 'Jorge Jesus', 'Coringas do Mengão', 157));
+  biblioteca.cadastrarMusica(Musica('Purple Haze', 'Jimi Hendrix', 'Are You Experienced', 209));
+  biblioteca.cadastrarMusica(Musica('Bohemian Rhapsody', 'Queen', 'A Night at the Opera', 355));
+  biblioteca.cadastrarMusica(Musica('Hotel California', 'Eagles', 'Hotel California', 391));
+
+  // Imprimir todas as músicas cadastradas
+  biblioteca.imprimirMusicas();
+
+  // Imprimir o número de músicas na biblioteca
+  print('Número de músicas: ${biblioteca.getNumeroMusicas()}');
+
+  // Imprimir o tempo total em horas das músicas da biblioteca
+  print('Tempo total em horas: ${biblioteca.getTempoTotalHoras()}');
+
+  // Busca de músicas pelo título, nome do artista e nome do álbum
+  print('\nBusca por título: ${biblioteca.buscarPorTitulo("Flamengo")}');
+  print('Busca por nome do artista: ${biblioteca.buscarPorArtista("Jimi Hendrix")}');
+  print('Busca por nome do álbum: ${biblioteca.buscarPorAlbum("A Night at the Opera")}');
 }
 
-class musicas {
-  late String titulo;
-  late String artista;
-  late String album;
-  late int duracao;
+class Musica {
+  final String titulo;
+  final String artista;
+  final String album;
+  final int duracao;
 
-  musicas(this.titulo, this.artista, this.album, this.duracao);
-
-  adicionarLista() {
-    List listaDasMusicas = [artista, album, duracao];
-    return listaDasMusicas;
-  }
+  Musica(this.titulo, this.artista, this.album, this.duracao);
 }
 
 class Biblioteca {
-  Map<String, List> listaDeMusicas = Map<String, List>();
+  final List<Musica> musicas = [];
 
-  adicionarMusicas(String musica, List informacoes) {
-    listaDeMusicas.addAll({
-      musica: [informacoes]
-    });
-    return listaDeMusicas;
+  void cadastrarMusica(Musica musica) {
+    musicas.add(musica);
+  }
+
+  void imprimirMusicas() {
+    for (var musica in musicas) {
+      print('Título: ${musica.titulo}');
+      print('Artista: ${musica.artista}');
+      print('Álbum: ${musica.album}');
+      print('Duração: ${musica.duracao} segundos');
+      print('---');
+    }
+  }
+
+  int getNumeroMusicas() {
+    return musicas.length;
+  }
+
+  double getTempoTotalHoras() {
+    int duracaoTotal = 0;
+    for (var musica in musicas) {
+      duracaoTotal += musica.duracao;
+    }
+    return duracaoTotal / 3600; // Converter para horas
+  }
+
+  List<Musica> buscarPorTitulo(String titulo) {
+    return musicas.where((musica) => musica.titulo.toLowerCase() == titulo.toLowerCase()).toList();
+  }
+
+  List<Musica> buscarPorArtista(String artista) {
+    return musicas.where((musica) => musica.artista.toLowerCase() == artista.toLowerCase()).toList();
+  }
+
+  List<Musica> buscarPorAlbum(String album) {
+    return musicas.where((musica) => musica.album.toLowerCase() == album.toLowerCase()).toList();
   }
 }
